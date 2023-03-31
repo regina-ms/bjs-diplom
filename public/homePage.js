4538f511f314a404f55e82659c6fcaf149171e2c
Regina <rmyachina@mail.ru>
2023-03-31 20:58:19 +0300

    Финальная версия

--- public/homePage.js
+++ public/homePage.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let userLogout = new LogoutButton;
 
 userLogout.action = () => {
@@ -9,7 +11,6 @@ userLogout.action = () => {
 } 
 
 ApiConnector.current(answer => {
-    //console.log(answer);
     if(answer.success) {
         ProfileWidget.showProfile(answer.data);
     }
@@ -17,8 +18,8 @@ ApiConnector.current(answer => {
 
 let rates = new RatesBoard;
 
-let showRatesBoard = () => {ApiConnector.getStocks(answer => {
-    //console.log(answer);
+let showRatesBoard = () => {
+    ApiConnector.getStocks(answer => {
     if(answer.success) {
         rates.clearTable();
         rates.fillTable(answer.data);
@@ -27,3 +28,69 @@ let showRatesBoard = () => {ApiConnector.getStocks(answer => {
 
 showRatesBoard();
 setInterval(showRatesBoard, 60000);
+
+let money = new MoneyManager;
+
+money.addMoneyCallback = (cash) => {
+    ApiConnector.addMoney(cash, answer => { 
+        if(answer.success) {
+            ProfileWidget.showProfile(answer.data);
+            answer.error = "Успех";
+        } 
+        money.setMessage(answer.success, answer.error);
+    })
+}
+
+money.conversionMoneyCallback = (balance) => {
+    ApiConnector.convertMoney (balance, answer => {
+        if(answer.success) {
+            ProfileWidget.showProfile(answer.data);
+            answer.error = "Успех";
+        } 
+        money.setMessage(answer.success, answer.error);
+    })
+}
+
+money.sendMoneyCallback = (amount) => {
+    ApiConnector.transferMoney(amount, answer => {
+        if(answer.success) {
+            ProfileWidget.showProfile(answer.data);
+            answer.error = "Успех";
+        } 
+        money.setMessage(answer.success, answer.error);
+    })
+}
+
+let favorite = new FavoritesWidget;
+
+ApiConnector.getFavorites(answer => {
+    if (answer.success) {
+        favorite.clearTable();
+        favorite.fillTable(answer.data);
+        money.updateUsersList(answer.data);
+    }
+});
+
+favorite.addUserCallback = (data) => {
+    ApiConnector.addUserToFavorites(data, answer => {
+        if(answer.success) {
+            favorite.clearTable();
+            favorite.fillTable(answer.data);
+            money.updateUsersList(answer.data);
+            answer.error = "Успех";
+        } 
+        favorite.setMessage(answer.success, answer.error);
+    })
+}
+
+favorite.removeUserCallback = (data) => {
+    ApiConnector.removeUserFromFavorites(data, answer => {
+        if(answer.success) {
+            favorite.clearTable();
+            favorite.fillTable(answer.data);
+            money.updateUsersList(answer.data);
+            answer.error = "Успех";
+        } 
+        favorite.setMessage(answer.success, answer.error);
+    })
+}
